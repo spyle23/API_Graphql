ebbaff3028f5f24cf10f76dd72155320762d62b2
spyle23 <sampingyen.lee@gmail.com>
2023-02-03 13:24:17 +0300

    fix package and add metadata config to fix bug

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
+import "reflect-metadata"
 import express from "express";
+import * as tq from "type-graphql";
+import resolvers from "./resolvers";
 import cors from "cors";
 import { createServer } from "http";
+import { execute, subscribe } from "graphql";
+import { ApolloServer } from "apollo-server-express";
+import { SubscriptionServer } from "subscriptions-transport-ws";
 
 require("dotenv").config();
 
@@ -11,10 +17,25 @@ const PORT = process.env.PORT || 4000;
   app.use(express.json({ limit: "50mb" }));
 
   const httpServer = createServer(app);
+  const schema = await tq.buildSchema({ resolvers, emitSchemaFile: true });
 
+  const server = new ApolloServer({
+    schema,
+    introspection: true,
+  });
 
-  httpServer.listen(PORT, ()=>{
-    console.log("mandeha le server")
-  })
-
+  await server.start();
+  server.applyMiddleware({ app, cors: false });
+  SubscriptionServer.create(
+    { schema, execute, subscribe },
+    { server: httpServer, path: server.graphqlPath }
+  );
+  httpServer.listen(PORT, () => {
+    console.log(
+      `🚀 Query endpoint ready at http://localhost:${PORT}${server.graphqlPath}`
+    );
+    console.log(
+      `🚀 Subscription endpoint ready at ws://localhost:${PORT}${server.graphqlPath}`
+    );
+  });
 })();
